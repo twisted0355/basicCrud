a7d97ee7ec3ae2920599c7922bc0ef435b1b0e6c
mikhawa <michaeljpitz@gmail.com>
2019-04-25 17:19:10 +0200

    request with triple selectok

--- datas/nos_requetes.sql
+++ datas/nos_requetes.sql
@@ -57,20 +57,36 @@ SELECT * FROM rubrique WHERE idrubrique=1;
 
 
 # les articles présents dans une rubrique par date descendante, avec l'auteur, lorsqu'ils sont visibles
-SELECT article.idarticle, article.idarticle AS sous, article.thetitle,LEFT(article.thetext,300) AS thetext, article.thedate,
-	   users.thename,
-       GROUP_CONCAT(rubrique.idrubrique ORDER BY rubrique.theintitule) AS idrubrique, 
-       GROUP_CONCAT(rubrique.theintitule ORDER BY rubrique.theintitule SEPARATOR '|@|') AS theintitule
-	FROM article
-    INNER JOIN users
-		ON users.idusers = article.users_idusers
-    LEFT JOIN article_has_rubrique
-		ON article_has_rubrique.article_idarticle = article.idarticle
-    LEFT JOIN rubrique
-		ON article_has_rubrique.rubrique_idrubrique = rubrique.idrubrique
-	WHERE article.thevisibility=1 AND article_has_rubrique.rubrique_idrubrique IN(5,(SELECT article_has_rubrique.rubrique_idrubrique FROM article_has_rubrique
-    WHERE  article_has_rubrique.rubrique_idrubrique!=5
-    AND sous = article_has_rubrique.article_idarticle))
-    GROUP BY article.idarticle
-    ORDER BY article.thedate DESC
+SELECT a.idarticle, a.thetitle,LEFT(a.thetext,300) AS thetext, a.thedate,
+	   u.thename,
+       GROUP_CONCAT(r.idrubrique ORDER BY r.theintitule) AS idrubrique, 
+       GROUP_CONCAT(r.theintitule ORDER BY r.theintitule SEPARATOR '|@|') AS theintitule
+	FROM article a
+    INNER JOIN users u
+		ON u.idusers = a.users_idusers
+    LEFT JOIN article_has_rubrique h
+		ON h.article_idarticle = a.idarticle
+    LEFT JOIN rubrique r
+		ON h.rubrique_idrubrique = r.idrubrique
+	WHERE a.thevisibility=1
+        AND h.rubrique_idrubrique IN 
+			(SELECT ha.rubrique_idrubrique FROM article_has_rubrique ha WHERE ha.article_idarticle = 
+				(SELECT a.idarticle FROM rubrique r
+					INNER JOIN article_has_rubrique h 
+						ON h.rubrique_idrubrique = r.idrubrique
+					INNER JOIN article a 
+						ON a.idarticle = h.article_idarticle
+					WHERE h.rubrique_idrubrique=5 
+                    ORDER BY r.theintitule ASC)
+            )
+    GROUP BY a.idarticle
+    ORDER BY a.thedate DESC
 ;
+
+
+SELECT a.idarticle FROM rubrique r
+INNER JOIN article_has_rubrique h 
+	ON h.rubrique_idrubrique = r.idrubrique
+INNER JOIN article a 
+	ON a.idarticle = h.article_idarticle
+ WHERE h.rubrique_idrubrique=5 ORDER BY r.theintitule ASC;
